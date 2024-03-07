3038a60f47a9e9ef147c89845957f8017ea15cf1
luong tran <luongluong99783@gmail.com>
2024-03-07 16:57:38 +0700

    footer

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,7 +1,6 @@
 import {  HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import {MatIconModule, MatIconRegistry} from '@angular/material/icon'
-import { DomSanitizer } from '@angular/platform-browser';
+import {MatIconModule} from '@angular/material/icon'
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -10,17 +9,5 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
- private iconConfigs:{name:string,path:string}[]=[
-    {name:'article-icon',path:'assets/images/article.svg'},
-    {name:'people-icon',path:'assets/images/people.svg'},
-    {name:'play-icon',path:'assets/images/play-button.svg'},
-    {name:'job-icon',path:'assets/images/job.svg'},
-    {name:'app-icon',path:'assets/images/app.svg'},
-  ]
-  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
-    this.iconConfigs.forEach(icon => this.registerIcon(icon.name,icon.path))
-  }
- private registerIcon(iconName:string, iconPath:string):void{
-    this.matIconRegistry.addSvgIcon(iconName,this.domSanitizer.bypassSecurityTrustResourceUrl(iconPath))
-  }
+
 }

2ea95497118a8c88c7e7b2f92785b59658dad405
luong tran <luongluong99783@gmail.com>
2024-03-19 15:22:18 +0700

    home_page_p5

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CarouselComponent } from 'app/components/carousel/carousel.component';
+import { ColleaguesComponent } from 'app/components/colleagues/colleagues.component';
+import { ConnectComponent } from 'app/components/connect/connect.component';
 import { DiscoverComponent } from 'app/components/discover/discover.component';
 import { PostJobComponent } from 'app/components/post-job/post-job.component';
 import { SelectionComponent } from 'app/components/selection/selection.component';
@@ -8,7 +10,15 @@ import { WelcomeComponent } from 'app/components/welcome/welcome.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [WelcomeComponent,SelectionComponent,PostJobComponent,DiscoverComponent, CarouselComponent],
+  imports: [
+    WelcomeComponent,
+    SelectionComponent,
+    PostJobComponent,
+    DiscoverComponent, 
+    CarouselComponent,
+    ConnectComponent,
+    ColleaguesComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
